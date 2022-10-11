685f93c92031bd3e69254d191cfea07a27d066d6
ginnguyen123 <nhuunghia95@gmail.com>
2022-10-11 00:47:02 -0700

    [Thực hành] Hiển thị thời gian của hệ thống

---
+++ SystemTime/src/SystemTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date dateNow = new Date();
+        System.out.println("Date now:" +dateNow);
+    }
+}
