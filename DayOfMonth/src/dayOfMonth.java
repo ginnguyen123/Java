e579126694a5717815552ddaaa6fbe9bf622e894
ginnguyen123 <nhuunghia95@gmail.com>
2022-10-11 20:41:25 -0700

    [Thực hành] Tính số ngày trong tháng

---
+++ DayOfMonth/src/dayOfMonth.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class dayOfMonth {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int month;
+        System.out.println("Nhập số tháng muốn kiếm tra");
+        month = scanner.nextInt();
+        switch (month){
+            case 2:
+                System.out.println("Tháng có 28/29 ngày!");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("Tháng " +month+" có 31 ngày!");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("Tháng "+month+" có 30 ngày!");
+                break;
+            default:
+                System.out.println("Nhập thông tin không đúng!");
+                break;
+        }
+    }
+}
